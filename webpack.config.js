b13deb012af0e40132b477f295c8990a090b86ca
Enrique Rodriguez <juanenriquerodriguezgarcia87@gmail.com>
2021-09-15 18:38:36 +0200

    cambio webpack a url-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path")
 
 module.exports = {
+  entry: path.resolve(__dirname, "src/index.js" ),
   output: {
-    filename: "app.bundle.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
+  
   module: {
     rules: [
       {
@@ -20,9 +23,7 @@ module.exports = {
       {
         test: /\.png/,
         exclude: /node_modules/,
-        use: {
-          loader: "file-loader",
-        },
+        use: "url-loader"
       },
     ],
   },
